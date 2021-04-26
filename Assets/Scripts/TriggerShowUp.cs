ce4824b9006c218bf6577e67f7fd2b7c5ee57637
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-04-27 01:09:40 +0200

    Map setup 4/4 or 3/3, cant honestly tell
    Greatest game ever is almost finished!

---
+++ Assets/Scripts/TriggerShowUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerShowUp : MonoBehaviour
+{
+    [SerializeField] private GameObject _UIPopUp;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>() == true)
+        {
+            _UIPopUp.SetActive(true);
+        }
+    }
+}
