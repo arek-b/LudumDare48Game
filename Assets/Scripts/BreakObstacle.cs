5c511a613c4aa1f1961f8c9c5826185599fbe18a
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-04-26 17:13:41 +0200

    CaveMap Update -> 1/3
    1/3, 1/3, 1/3!

---
+++ Assets/Scripts/BreakObstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakObstacle : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<ObstacleToBreak>())
+        {
+            other.GetComponent<ObstacleToBreak>().Destruct();
+        }
+    }
+}
