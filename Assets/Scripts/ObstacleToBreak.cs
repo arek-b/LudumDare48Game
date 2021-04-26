bb8a405908ab8099f908732546a2e9ac47554141
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-04-26 23:48:38 +0200

    Some gameplay 2/3
    without an ending and one giant enemy non-crab

--- Assets/Scripts/ObstacleToBreak.cs
+++ Assets/Scripts/ObstacleToBreak.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class ObstacleToBreak : MonoBehaviour
 {
     [SerializeField] private GameObject _animatedObject;
+    [SerializeField] private bool _ifExploding = false;
+    [SerializeField] private GameObject _exploPrefab;
+    [SerializeField] private Transform _placeToExplode;
     public void Destruct()
     {
         _animatedObject.GetComponent<Animator>().enabled = true;
+        if (_ifExploding == true)
+        {
+            GameObject loot = Instantiate(_exploPrefab, transform.position, Quaternion.identity) as GameObject;
+            loot.transform.position = _placeToExplode.transform.position;
+        }
     }
 }
